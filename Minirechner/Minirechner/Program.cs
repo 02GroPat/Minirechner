dd2cbc6fc8808779ad3d52df6ba1b90aa34c9cb2
02GroPat <02gropat@rgtfo-me.it>
2020-04-21 21:54:51 +0200

    C# Programm erstellt und gespeichert

---
+++ Minirechner/Minirechner/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Gross Patrick
+//4BEL
+//Minirechner, welcher die Grundrechenarten von zwei eingegebenen Integer Zahlern durchführt
+namespace Minirechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
